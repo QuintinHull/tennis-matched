9bfdd7ca9bdc4ba8a4503dd9563fdcfdd753547c
Quintin Hull <quintinhull92@gmail.com>
2021-02-01 04:52:39 -0500

    done styling log in

--- frontend/src/components/EventView/index.js
+++ frontend/src/components/EventView/index.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
 import { getOneEvent } from "../../store/event";
 import { getComments, postComment } from "../../store/comment";
+import "./EventView.css";
 
 function EventView() {
   const { id } = useParams();
@@ -37,34 +38,55 @@ function EventView() {
   }
 
   return (
-    <div>
-      <h1>EventView Component</h1>
-      <p>{event && event.description}</p>
-      <p>{event && event.date}</p>
-      <p>{event && event.time}</p>
-      <p>{event && event.place}</p>
-      <p>{event.User && event.User.username}</p>
-      <p>{event.Group && event.Group.name}</p>
-      <p>{event.Skill && event.Skill.level}</p>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          name="message"
-        />
-        <button type="submit">Comment!</button>
-      </form>
-      <h1>Comments</h1>
-      {comments.map((comment) => {
-        return (
-          <div key={comment.id}>
-            <h3>{comment && comment.message}</h3>
-            <h4>{comment.User && comment.User.username}</h4>
+    <>
+      <div className="event_page">
+        <div className="event_view__container">
+          <div className="event_view__row1">
+            <div className="event__user_skill">
+              <h3>{event.User && event.User.username}</h3>
+              <h5>Skill: {event.Skill && event.Skill.level}</h5>
+            </div>
+            <div className="event__date_time">
+              <h5>{event && event.date}</h5>
+              <h5>{event && event.time}</h5>
+            </div>
           </div>
-        );
-      })}
-    </div>
+          <div className="event_view__row2">
+            <h1>{event && event.description}</h1>
+          </div>
+          <div className="event_view__row3">
+            <h5>{event && event.place}</h5>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                placeholder="leave a comment"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                name="message"
+              />
+              <button type="submit">comment</button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className="comments_container">
+        <h1 className="comments_title">Comments</h1>
+      </div>
+      <div className="comment_body__container">
+        {comments.map((comment) => {
+          return (
+            <div className="comment_body">
+              <div className="comment_body__message" key={comment.id}>
+                <h3>{comment && comment.message} </h3>
+              </div>
+              <div className="comment_body__user">
+                <h4> - {comment.User && comment.User.username}</h4>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
